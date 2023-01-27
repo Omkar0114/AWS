b7439cdba81799f942333ccc8ab8dfc7eab0bbf5
Omkar <opkulkarni0104@gmail.com>
2023-01-27 12:13:36 +0530

    added go server file

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"log"
+)
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 not found", http.StatusNotFound)
+		return
+	}
+	if r.Method != "GET" {
+		http.Error(w, "method is not supported", http.StatusNotFound)
+		return
+	}
+	fmt.Fprintf(w, "Hello, How are you?")
+}
+
+
+func main(){
+	fileServer := http.FileServer(http.Dir("./Static"))
+	http.Handle("/", fileServer)
+	
+	http.HandleFunc("/hello", helloHandler)
+
+	fmt.Printf("Starting server at port 8080\n")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
+}
